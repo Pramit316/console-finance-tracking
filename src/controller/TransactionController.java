842d8c394c25e30d8d3d61842b98665dcf484d3e
Pramit <primeshipbas@gmail.com>
2026-05-20 14:08:23 +0545

    Added custom exception to handle exceptions

--- src/controller/TransactionController.java
+++ src/controller/TransactionController.java
@@ -2,6 +2,9 @@ package controller;
 
 import entity.Transaction;
 import entity.TransactionType;
+import exceptions.InvalidAmountException;
+import exceptions.InvalidTransactionTypeException;
+import exceptions.TransactionNotFoundException;
 import service.TransactionService;
 
 import java.time.LocalDate;
@@ -20,40 +23,45 @@ public class TransactionController {
         int id = transactionService.generateId();
         t.setId(id);
 
-        System.out.println("\n----------------------------------------");
-        System.out.println("           ADD NEW TRANSACTION");
-        System.out.println("----------------------------------------");
+        try{
+            System.out.println("\n----------------------------------------");
+            System.out.println("           ADD NEW TRANSACTION");
+            System.out.println("----------------------------------------");
 
-        System.out.println("Generated Transaction ID: " + id);
+            System.out.println("Generated Transaction ID: " + id);
 
-        System.out.print("Enter Title: ");
-        String title = sc.nextLine();
-        t.setTitle(title);
+            System.out.print("Enter Title: ");
+            String title = sc.nextLine();
+            t.setTitle(title);
 
-        System.out.print("Enter Amount: ");
-        double amount = sc.nextDouble();
-        sc.nextLine();
-        t.setAmount(amount);
+            System.out.print("Enter Amount: ");
+            double amount = sc.nextDouble();
+            sc.nextLine();
+            t.setAmount(amount);
 
-        System.out.println("Select Category: ");
-        System.out.println("1. Income");
-        System.out.println("2. Expense");
-        System.out.print("Enter choice: ");
-        int category = sc.nextInt();
-        sc.nextLine();
+            System.out.println("Select Category: ");
+            System.out.println("1. Income");
+            System.out.println("2. Expense");
+            System.out.print("Enter choice: ");
+            int category = sc.nextInt();
+            sc.nextLine();
 
-        t.setCategory(String.valueOf(category == 1 ? TransactionType.INCOME : TransactionType.EXPENSE));
+            t.setCategory(String.valueOf(category == 1 ? TransactionType.INCOME : TransactionType.EXPENSE));
 
-        LocalDate currentDate = LocalDate.now();
-        t.setDate(currentDate);
+            LocalDate currentDate = LocalDate.now();
+            t.setDate(currentDate);
 
-        System.out.print("Enter Description: ");
-        String description = sc.nextLine();
-        t.setDescription(description);
+            System.out.print("Enter Description: ");
+            String description = sc.nextLine();
+            t.setDescription(description);
 
-        transactionService.addTransaction(t);
+            transactionService.addTransaction(t);
 
-        System.out.println("\nTransaction added successfully!");
+            System.out.println("\nTransaction added successfully!");
+        } catch (Exception e) {
+            System.out.println("\nSomething went wrong. Please enter valid input");
+            pause();
+        }
     }
 
     public void transactionMenu() {
@@ -139,15 +147,19 @@ public class TransactionController {
         System.out.println("         FIND TRANSACTION BY ID");
         System.out.println("----------------------------------------");
 
-        System.out.print("Enter Transaction ID: ");
-        int id = sc.nextInt();
-        sc.nextLine();
+        try{
+            System.out.print("Enter Transaction ID: ");
+            int id = sc.nextInt();
+            sc.nextLine();
 
-        Transaction t = transactionService.findTransactionById(id);
+            Transaction t = transactionService.findTransactionById(id);
 
-        System.out.println("\nSearching transaction with ID: " + id);
+            System.out.println("\nSearching transaction with ID: " + id);
 
-        consolePrint(t);
+            consolePrint(t);
+        } catch (TransactionNotFoundException e){
+            System.out.println("\nError: " + e.getMessage());
+        }
     }
 
     public void showDeleteTransactionScreen() {
@@ -171,26 +183,104 @@ public class TransactionController {
         System.out.println("           UPDATE TRANSACTION");
         System.out.println("----------------------------------------");
 
-        System.out.print("Enter Transaction ID to update: ");
-        int id = sc.nextInt();
+       try{
+           System.out.print("Enter Transaction ID to update: ");
+           int id = sc.nextInt();
+           sc.nextLine();
+
+           System.out.println("\nWhat do you want to update?");
+           System.out.println("1. Title");
+           System.out.println("2. Amount");
+           System.out.println("3. Category");
+           System.out.println("4. Description");
+           System.out.println("5. Update Full Transaction");
+           System.out.println("6. Cancel");
+           System.out.print("Enter your choice: ");
+
+           int updateChoice = sc.nextInt();
+           sc.nextLine();
+
+           switch (updateChoice){
+               case 1:
+                   System.out.print("Enter the new title: ");
+                   String title = sc.nextLine();
+                   transactionService.updateTitle(id, title);
+                   break;
+
+               case 2:
+                   System.out.print("Enter the new amount: ");
+                   double amount = sc.nextDouble();
+                   transactionService.updateAmount(id, amount);
+                   break;
+
+               case 3:
+                   System.out.print("Select the category: ");
+                   System.out.println("1. Income");
+                   System.out.println("2. Expense");
+                   System.out.print("Enter your choice");
+                   int input = sc.nextInt();
+                   sc.nextLine();
+
+                   transactionService.updateCategory(id, input == 1 ? TransactionType.INCOME : TransactionType.EXPENSE);
+                   break;
+
+               case 4:
+                   String description = sc.nextLine();
+                   transactionService.updateDescription(id, description);
+                   break;
+
+               case 5:
+                   updateFullTransaction(id);
+           }
+
+           System.out.println("\nUpdate request received for transaction ID: " + id);
+           System.out.println("Selected update option: " + updateChoice);
+       } catch(InvalidAmountException | TransactionNotFoundException | InvalidTransactionTypeException exception){
+           System.out.println("\nError: " + exception.getMessage());
+       } catch (Exception e){
+           System.out.println("\nSomething went wrong. Please enter the valid input.");
+       }
+    }
+
+    private void updateFullTransaction(int id) {
+
+        Transaction t = new Transaction();
+        t.setId(id);
+
+        System.out.println("\n----------------------------------------");
+        System.out.println("           UPDATE TRANSACTION");
+        System.out.println("----------------------------------------");
+
+        System.out.println("Generated Transaction ID: " + id);
+
+        System.out.print("Enter Title: ");
+        String title = sc.nextLine();
+        t.setTitle(title);
+
+        System.out.print("Enter Amount: ");
+        double amount = sc.nextDouble();
+        sc.nextLine();
+        t.setAmount(amount);
+
+        System.out.println("Select Category: ");
+        System.out.println("1. Income");
+        System.out.println("2. Expense");
+        System.out.print("Enter choice: ");
+        int category = sc.nextInt();
         sc.nextLine();
 
-        System.out.println("\nWhat do you want to update?");
-        System.out.println("1. Title");
-        System.out.println("2. Amount");
-        System.out.println("3. Category");
-        System.out.println("4. Description");
-        System.out.println("5. Update Full Transaction");
-        System.out.println("6. Cancel");
-        System.out.print("Enter your choice: ");
+        t.setCategory(String.valueOf(category == 1 ? TransactionType.INCOME : TransactionType.EXPENSE));
 
-        int updateChoice = sc.nextInt();
-        sc.nextLine();
+        LocalDate currentDate = LocalDate.now();
+        t.setDate(currentDate);
 
-        // transactionService.updateTransaction(id, updatedTransaction);
+        System.out.print("Enter Description: ");
+        String description = sc.nextLine();
+        t.setDescription(description);
 
-        System.out.println("\nUpdate request received for transaction ID: " + id);
-        System.out.println("Selected update option: " + updateChoice);
+        transactionService.updateEntireTransaction(t);
+
+        System.out.println("\nTransaction added successfully!");
     }
 
     public void pause() {
@@ -198,6 +288,7 @@ public class TransactionController {
         sc.nextLine();
     }
 
+    //single result in a table
     public void consolePrint(Transaction t){
         System.out.printf("%-5s %-15s %-10s %-15s %-15s %-20s%n",
                 "ID", "TITLE", "AMOUNT", "CATEGORY", "DATE", "DESCRIPTION");
@@ -212,6 +303,8 @@ public class TransactionController {
                 t.getDescription());
     }
 
+
+    //List of results in a table
     public void consolePrint(List<Transaction> transactionList){
         System.out.printf("%-5s %-15s %-10s %-15s %-15s %-20s%n",
                 "ID", "TITLE", "AMOUNT", "CATEGORY", "DATE", "DESCRIPTION");
--- src/service/TransactionService.java
+++ src/service/TransactionService.java
@@ -1,6 +1,9 @@
 package service;
 
 import entity.Transaction;
+import entity.TransactionType;
+import exceptions.InvalidAmountException;
+import exceptions.TransactionNotFoundException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +18,10 @@ public class TransactionService {
     }
 
     public void addTransaction(Transaction transaction){
+        if(transaction.getAmount() <= 0){
+            throw new InvalidAmountException("Amount must be greater than zero.");
+        }
+
         transactionList.add(transaction);
     }
 
@@ -23,6 +30,43 @@ public class TransactionService {
     }
 
     public Transaction findTransactionById(int id) {
-        return transactionList.stream().filter(x-> x.getId() == id).findFirst().orElse(null);
+        return transactionList.stream().filter(x-> x.getId() == id).findFirst().orElseThrow(() -> new TransactionNotFoundException("Transaction not found with id: " + id));
+    }
+
+    public void updateTitle(int id, String title){
+        Transaction t = findTransactionById(id);
+        t.setTitle(title);
+    }
+
+    public void updateAmount(int id, double amount) {
+        if(amount <= 0){
+            throw new InvalidAmountException("Amount must be greater than zero.");
+        }
+        Transaction t = findTransactionById(id);
+        t.setAmount(amount);
+    }
+
+    public void updateCategory(int id, TransactionType transactionType) {
+        Transaction t = findTransactionById(id);
+        t.setCategory(String.valueOf(transactionType));
+    }
+
+    public void updateDescription(int id, String description) {
+        Transaction t = findTransactionById(id);
+        t.setDescription(description);
+    }
+
+    public void updateEntireTransaction(Transaction t) {
+        if(t.getAmount() <= 0){
+            throw new InvalidAmountException("Amount must be greater than zero.");
+        }
+
+        Transaction tNew = findTransactionById(t.getId());
+
+        tNew.setTitle(t.getTitle());
+        tNew.setAmount(t.getAmount());
+        tNew.setDescription(t.getDescription());
+        tNew.setType(t.getType());
+        tNew.setCategory(t.getCategory());
     }
 }
